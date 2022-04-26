4adeb183836712bba8a776ebdceb19a5e940d9ef
L1Gross <profgross@gmail.com>
2022-04-26 12:24:48 -0400

    Store contents of layout container as ObservableList

--- src/main/java/bsu/comp152/Main.java
+++ src/main/java/bsu/comp152/Main.java
@@ -1,73 +1,97 @@
 package bsu.comp152;
 
 import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
+import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
+import javafx.geometry.Pos;
+import javafx.geometry.Insets;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+import javafx.event.EventHandler;
+import javafx.event.ActionEvent;
 
-import java.util.Random;
+/**
+ *  Kilometer Converter application
+ */
 
-public class Main extends Application {
+public class Main extends Application
+{
+    private Label resultLabel;
+    private TextField kiloTextField;
 
-    public static void main(String[] args) {
-
-        // launch the application
+    public static void main(String[] args)
+    {
+        // Launch the application.
         launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage){
+    public void start(Stage primaryStage)
+    {
+        // Declare and create a Label to display a prompt.
+        Label promptLabel = new Label("Enter a distance in kilometers:");
 
-        // Create components.
-        Button centerButton = new Button("This is the center");
-        Label topLabel = new Label("This is a label1.");
-        // Create a label component.
-        Label bottomLabel = new Label("This is a label2.");
-        // Create a label component.
-        Label leftLabel = new Label("This is a label3.");
-        // Create a label component.
-        Label rightLabel = new Label("This is a label4.");
+        // Create a TextField for input.
+        kiloTextField = new TextField();
 
-        // Add each control into a container.
-        HBox centerBox = new HBox(centerButton);
-        HBox topBox = new HBox(topLabel);
-        HBox bottomBox = new HBox(bottomLabel);
-        VBox leftBox = new VBox(leftLabel);
-        VBox rightBox = new VBox(rightLabel);
+        // Declare and create a Button to perform the conversion.
+        Button calcButton = new Button("Convert");
 
-        topBox.setAlignment(Pos.CENTER);
-        bottomBox.setAlignment(Pos.CENTER);
+        // Register the event handler.
+        calcButton.setOnAction(new CalcButtonHandler());
 
-        // Put the labels in a BorderPane layout container.
+        // Create an empty Label to display the result.
+        resultLabel = new Label();
 
-        BorderPane borderpane = new BorderPane();
+        // Put the promptLabel and the kiloTextField in an HBox.
+        HBox hbox = new HBox(10, promptLabel);
 
-        borderpane.setCenter(centerBox);
-        borderpane.setTop(topBox);
-        borderpane.setBottom(bottomBox);
-        borderpane.setLeft(leftBox);
-        borderpane.setRight(rightBox);
+        // Store the contents of hbox as an ObservableList:
+        var contents = hbox.getChildren();
+        // You can add more contents with
+        contents.add(kiloTextField);
 
-        // Put the BorderPane in a Scene.
-        // Scene scene = new Scene(vbox);
-        // Or make the scene 500 pixels wide by 200 pixels high.
-        // Create a Scene with the BorderPane as its root node.
-        Scene scene = new Scene(borderpane, 500, 200);
+        // Put the HBox, calcButton, and resultLabel in a VBox.
+        var vbox = new VBox(10, hbox, calcButton, resultLabel);
+
+        // Set the VBox's alignment to center.
+        vbox.setAlignment(Pos.CENTER);
+
+        // Set the VBox's padding to 10 pixels.
+        vbox.setPadding(new Insets(10));
+
+        // Declare and create a Scene.
+        var scene = new Scene(vbox);
 
         // Add the Scene to the Stage.
         primaryStage.setScene(scene);
 
-        // Set the window's title.
-        primaryStage.setTitle("My First BorderPane");
+        // Set the stage title.
+        primaryStage.setTitle("Kilometer Converter");
 
         // Show the window.
         primaryStage.show();
+    }
+
+    /*
+     * Event handler class for calcButton
+     */
+    class CalcButtonHandler implements EventHandler<ActionEvent>
+    {
+        @Override
+        public void handle(ActionEvent event)
+        {
+            // Get the kilometers.
+            double kilometers = Double.parseDouble(kiloTextField.getText());
+
+            // Convert the kilometers to miles.
+            double miles = kilometers * 0.6214;
+
+            // Display the results.
+            resultLabel.setText(String.format("%,.2f miles", miles));
+        }
     }
 }
